4a5fb46c34cdf5c2725eab49e6d336ddfaa31924
stecaulab <cauzzistefano@icloud.com>
2022-01-24 21:08:02 +0100

    refactotry code headersecond

--- app/View/Components/headersecond.php
+++ app/View/Components/headersecond.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class header-second extends Component
+class headersecond extends Component
 {
     /**
      * Create a new component instance.
