29aec95610aafc60eee57ccaa59013679d5d9bd9
stecaulab <cauzzistefano@icloud.com>
2022-11-16 20:01:05 +0100

    add coolaborazioni+style

--- resources/views/components/collaborazioni.blade.php
+++ resources/views/components/collaborazioni.blade.php
@@ -9,7 +9,7 @@
 
             <h1
                 class="pt-4 top-10 italic text-3xl font-semibold text-center inline-block text-[#5d514c] md:w-full md:text-7xl  ">
-                Noi Siamo quello che Mangiamo <br> ed è proprio così!
+                Noi Siamo quello che Mangiamo <br> ed è proprio così.
 
             </h1>
 
@@ -17,39 +17,208 @@
     </div>
 </div>
 
+
 <div class="container mx-auto   text-gray-500 md:max-w-5xl md:mt-10">
 
-    <p class="ml-5 text-left mt-3 leading-loose md:text-lg">
+    <p class="ml-5 mr-5 text-left text-lg mt-3 leading-loose md:text-lg">
         Offriamo l'esperienza di divulgare insieme a noi un messaggio di agricoltura etica e sostenibile, perchè amiamo
         la terra in cui viviamo. <br>
     </p>
 
-    <p class="ml-5 text-left mt-3 leading-loose md:text-lg">
-        Per questo è importante portare sulle nostre tavole alimenti coltivati nel rispettodella terra e di tutti i suoi
+    <p class="ml-5 mr-5 text-left text-lg mt-3 leading-loose md:text-lg">
+        Per questo è importante portare sulle nostre tavole alimenti coltivati nel rispetto della terra e di tutti i
+        suoi
         abitanti. <br>
         Solo un terreno sano e ricco di biodiversità dona prodotti di buona qualità. <br>
-        Costruiamo insieme un mondo sotenibile. <br>
+        Costruiamo insieme un mondo sostenibile. <br>
     </p>
 
-    <p class="ml-5 text-left mt-3 leading-loose md:text-lg">
-        Grazie all'intuizione culinaria dello chef <a href="">Andrea Matasoglio</a> vi proponiamo la ricetta di un
-        dolce allo zafferano. <br>
-        Potrete creare un piatto in grado di emozionare e che abbia una storia da raccontare. <br>
-        Diamo il vantaggio di lavorare con materie prime almassimo della purezza e che si trasmettenei piatti anche con
-        una minima quantità di prodotto. <br>
-    </p>
-
-    <p class="ml-5 text-left mt-3 leading-loose md:text-lg">
-        Ed è 100% ITALIANO!!
-    </p>
-
-    <div class="flex justify-between items-center">
-        <div class="mt-28 border bg-cover border-red-800">
-            <img src="/img/andrea.jpg" alt="">
+    <div class="flex justify-between items-start">
+        <div class="">
+            <h2 class="uppercase font-extrabold text-[#870058] ml-5 text-left text-2xl mt-3 leading-loose">presentazione
+            </h2>
+            <p class="ml-5 text-left text-lg mt-2 leading-loose md:text-lg mr-5">
+                Grazie all'intuizione culinaria dello chef <a href=""><b>Andrea Mattasoglio</b></a> vi proponiamo
+                la
+                ricetta
+                di un
+                dolce allo zafferano. <br>
+                Potrete creare un piatto in grado di emozionare e che abbia una storia da raccontare. <br>
+                Diamo il vantaggio di lavorare con materie prime al massimo della purezza e che si trasmette nei piatti
+                anche
+                con
+                una minima quantità di prodotto. <br>
+            </p>
+            <p class="ml-5 text-left text-lg mt-3 leading-loose md:text-lg">
+                Ed è 100% <b>ITALIANO!!</b>
+            </p>
         </div>
-        <div class="mt-28 border bg cover border-red-800">
-            <img src="/img/dolce_zafferano.jpg" alt="">
-        </div>
+
 
     </div>
+    <div>
+        <div>
+            <h2 class="uppercase font-extrabold text-2xl text-[#870058] ml-5 text-left mt-3 leading-loose">sfere allo
+                zafferano</h2>
+        </div>
+
+        <div class="flex flex-col md:flex-row">
+            <div class="mt-3 md:h-[580px] md:w-[750px]">
+                <img src="/img/sfere_di_zafferano.jpg" alt="" class="md:ml-1 md:mr-1">
+            </div>
+
+            <div>
+                <ul class="ml-5 mt-8 flex flex-col">
+                    <li class="mt-2 mb-2"> <span class="inline-block ">
+                            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                viewBox="0 0 219.792 219.792" style="enable-background:new 0 0 219.792 219.792;"
+                                xml:space="preserve" class="h-8 w-8">
+                                <g>
+                                    <path
+                                        d="M159.4,25.975c-0.916,0-1.836,0.024-2.759,0.07C146.664,9.856,129.195,0,109.896,0c-19.3,0-36.769,9.856-46.744,26.045
+                           c-0.923-0.047-1.844-0.07-2.76-0.07c-30.306,0-54.961,24.656-54.961,54.962c0,25.355,17.397,47.182,41.432,53.264v78.092
+                           c0,4.143,3.357,7.5,7.5,7.5h111.066c4.143,0,7.5-3.357,7.5-7.5v-78.088c24.031-6.084,41.432-27.917,41.432-53.268
+                           C214.361,50.631,189.706,25.975,159.4,25.975z M157.929,204.792H61.863v-14.136h20.639h27.395h27.396h20.638V204.792z
+                            M164.485,120.563c-3.747,0.476-6.556,3.663-6.556,7.44v50.152h-14.388v-46.779c0-3.452-2.798-6.25-6.25-6.25
+                           c-3.452,0-6.25,2.798-6.25,6.25v46.779h-14.896v-46.779c0-3.452-2.798-6.25-6.25-6.25c-3.452,0-6.25,2.798-6.25,6.25v46.779H88.752
+                           v-46.779c0-3.452-2.798-6.25-6.25-6.25c-3.452,0-6.25,2.798-6.25,6.25v46.779H61.863V128.73c0.008-0.085,0.015-0.17,0.021-0.255
+                           c0.248-3.952-2.618-7.416-6.547-7.912c-19.899-2.51-34.905-19.546-34.905-39.626c0-22.035,17.927-39.962,39.961-39.962
+                           c1.929,0,3.893,0.144,5.842,0.431c3.221,0.472,6.374-1.184,7.813-4.102C80.837,23.546,94.575,15,109.896,15
+                           c15.32,0,29.058,8.547,35.852,22.305c1.439,2.917,4.587,4.571,7.814,4.101c1.944-0.286,3.908-0.431,5.838-0.431
+                           c22.034,0,39.961,17.927,39.961,39.962C199.361,101.005,184.368,118.04,164.485,120.563z"
+                                        fill="#dda704" />
+                                </g>
+
+                            </svg>
+                        </span>
+                        <span class="inline-block ml-2 text-lg text-black font-thin align-top mt-2">Difficoltà:
+                            <strong>Media</strong> </span>
+                    </li>
+                    <li class="mt-2 mb-2"> <span class="inline-block ">
+                            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                viewBox="0 0 511.999 511.999" style="enable-background:new 0 0 511.999 511.999;"
+                                xml:space="preserve" class="h-8 w-8">
+                                <g>
+
+                                    <path
+                                        d="M370.901,248.589c0.003,0,0.008,0,0.011,0c19.789-0.003,38.392-7.711,52.381-21.7l81.473-81.474
+                               c9.643-9.643,9.643-25.277,0-34.92s-25.277-9.643-34.92,0l-81.474,81.474c-4.664,4.664-10.868,7.233-17.468,7.235
+                               c-6.598,0.002-12.799-2.566-17.461-7.228c-4.662-4.66-7.228-10.861-7.227-17.459c0.002-6.599,2.571-12.802,7.235-17.466
+                               l81.477-81.474c9.642-9.643,9.643-25.277,0-34.92c-9.642-9.642-25.277-9.643-34.92,0l-81.479,81.474
+                               c-13.989,13.989-21.697,32.589-21.7,52.376c-0.003,11.247,2.491,22.108,7.205,31.959l-42.302,42.302L71.083,58.115
+                               c-9.642-9.642-25.277-9.643-34.92-0.002C12.843,81.434,0,112.438,0,145.416c-0.002,32.978,12.842,63.984,36.162,87.303
+                               l68.43,68.428c9.642,9.642,25.279,9.643,34.92-0.002l52.381-52.381l34.922,34.92L74.078,436.422
+                               c-9.643,9.643-9.643,25.277,0,34.92c4.822,4.822,11.141,7.233,17.459,7.233c6.32,0,12.639-2.412,17.459-7.233l152.735-152.735
+                               l135.276,135.276c4.822,4.822,11.141,7.233,17.459,7.233c6.32,0,12.639-2.412,17.459-7.233c9.643-9.643,9.643-25.277,0-34.92
+                               L296.651,283.687l42.303-42.302C348.805,246.096,359.658,248.589,370.901,248.589z M122.052,248.766l-50.971-50.969
+                               c-13.991-13.991-21.697-32.594-21.697-52.381c0-11.243,2.487-22.105,7.204-31.956l100.382,100.384L122.052,248.766z"
+                                        fill="#dda704" />
+                                </g>
+
+                            </svg>
+                        </span>
+                        <span class="inline-block ml-2 text-lg text-black font-thin align-top mt-2">Preparazione:
+                            <strong>3 ore</strong> </span>
+                    </li>
+                    <li class="mt-2 mb-2"> <span class="inline-block ">
+                            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                viewBox="0 0 296.998 296.998" style="enable-background:new 0 0 296.998 296.998;"
+                                xml:space="preserve" class="h-8 w-8">
+                                <path
+                                    d="M296.998,131.288c0-15.047-12.238-27.288-27.281-27.288h-7.355V88.334c0-14.288-11.623-25.911-25.91-25.911h-57.342
+                       c-1.426-15.64-14.607-27.935-30.611-27.935s-29.184,12.296-30.609,27.935H60.545c-14.289,0-25.912,11.623-25.912,25.911V104h-7.35
+                       C12.238,104,0,116.241,0,131.288c0,13.674,10.113,25.029,23.256,26.982l-7.012,28.048c-5.002,20.016-1.496,39.057,9.871,53.616
+                       c11.367,14.559,28.99,22.576,49.623,22.576h145.52c20.633,0,38.256-8.018,49.623-22.576c11.367-14.559,14.873-33.6,9.871-53.616
+                       l-7.012-28.048C286.883,156.319,296.998,144.964,296.998,131.288z M148.498,54.419c4.988,0,9.188,3.402,10.43,8.004h-20.859
+                       C139.311,57.821,143.51,54.419,148.498,54.419z M54.564,88.334c0-3.24,2.738-5.979,5.98-5.979h175.906
+                       c3.24,0,5.979,2.739,5.979,5.979V104H54.564V88.334z M28.15,138.683c-4.055,0-8.221-3.342-8.221-7.395
+                       c0-4.057,3.299-7.356,7.353-7.356h4.555L28.15,138.683z M255.172,227.667c-7.51,9.616-19.553,14.912-33.914,14.912H75.738
+                       c-14.361,0-26.404-5.296-33.914-14.911c-7.508-9.616-9.727-22.584-6.244-36.517l16.801-67.22h192.234l16.801,67.22
+                       C264.896,205.083,262.68,218.052,255.172,227.667z M268.846,138.683l-3.686-14.751h4.557c4.053,0,7.352,3.3,7.352,7.356
+                       C277.068,135.341,272.898,138.683,268.846,138.683z"
+                                    fill="#dda704" />
+
+                            </svg>
+                        </span>
+                        <span class="inline-block ml-2 text-lg text-black font-thin align-top mt-2">Freezer:
+                            <strong>2 ore</strong> </span>
+                    </li>
+                    <li class="mt-2 mb-2"> <span class="inline-block ">
+                            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                viewBox="0 0 511.999 511.999" style="enable-background:new 0 0 511.999 511.999;"
+                                xml:space="preserve" class="h-8 w-8">
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M101.556,48.777v0.001H86.34H60.064C26.945,48.778,0,75.722,0,108.842v171.766h30.432V108.842
+			c0-16.339,13.293-29.632,29.632-29.632H86.34v315.064c0,38.019,30.929,68.948,68.947,68.948h245.996
+			c38.017,0,68.946-30.93,68.946-68.948V124.191l41.769-75.414H101.556z M439.799,116.327v277.947
+			c0,21.239-17.277,38.516-38.514,38.516H155.288c-21.238,0-38.515-17.278-38.515-38.516V79.21h343.583L439.799,116.327z"
+                                            fill="#dda704" />
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <rect x="142.128" y="109.637" width="105.711" height="30.432"
+                                            fill="#dda704" />
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <rect x="142.128" y="160.367" width="75.786" height="30.432"
+                                            fill="#dda704" />
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <rect x="142.128" y="211.088" width="105.711" height="30.432"
+                                            fill="#dda704" />
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <rect x="142.128" y="261.798" width="75.786" height="30.432"
+                                            fill="#dda704" />
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <rect x="142.128" y="312.518" width="105.711"
+                                            height="30.432"fill="#dda704" />
+                                    </g>
+                                </g>
+
+                            </svg>
+                        </span>
+                        <span class="inline-block ml-2 text-lg text-black font-thin align-top mt-2">Dosi:
+                            <strong>6-8 sfere</strong> </span>
+                    </li>
+
+                    <li class="mt-2 mb-2"> <span class="inline-block ">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg" fill="#dda704" class="h-8 w-8">
+                                <g>
+                                    <path fill="none" d="M0 0h24v24H0z" fill="#dda704" />
+                                    <path
+                                        d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1.95-9H15v2h-4.95a2.5 2.5 0 0 0 4.064 1.41l1.7 1.133A4.5 4.5 0 0 1 8.028 13H7v-2h1.027a4.5 4.5 0 0 1 7.788-2.543L14.114 9.59A2.5 2.5 0 0 0 10.05 11z"
+                                        fill="#dda704" />
+                                </g>
+                            </svg>
+                        </span>
+                        <span class="inline-block ml-2 text-lg text-black font-thin align-top mt-2">Costo:
+                            <strong>Medio</strong> </span>
+                    </li>
+
+                </ul>
+
+
+            </div>
+        </div>
+    </div>
+
+    <h3 class="uppercase ml-5 font-extrabold text-xl text-[#870058] text-left mt-5 leading-loose "> ingredienti</h3>
 </div>
